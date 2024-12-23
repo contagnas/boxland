179c9887afe2e04ef44764af774f083cc21d78fa
contagnas <contagnas@gmail.com>
2024-12-23 04:16:25 -0500

    move game to component, start connecting websocket

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,29 +3,16 @@ import './App.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import { Header } from "@/components/header"
 import { BouncingBall } from "@/components/bouncing-ball"
-import init from "@/game/game"
-
-async function runGame() {
-  try {
-    await init();
-  } catch (e) {
-    const error = e as Error;
-    if (error.message.includes("Using exceptions for control flow")) {
-      return;
-    }
-    console.error("Failed to start game", error);
-  }
-}
+import { Game } from "@/components/game"
 
 function App() {
-  useEffect(() => {runGame()}, [])
 
   return (
     <ThemeProvider defaultTheme="dark">
       <div className="flex flex-col items-center h-full relative">
         <Header />
         <BouncingBall className="absolute top-0 left-0 w-full h-full z-[-1]" />
-        <canvas id="game" />
+        <Game />
       </div>
     </ThemeProvider>
   )
---
+++ web/src/components/game.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+import init from "@/game/game"
+
+
+async function runGame() {
+  try {
+    await init();
+  } catch (e) {
+    const error = e as Error;
+    if (error.message.includes("Using exceptions for control flow")) {
+      return;
+    }
+    console.error("Failed to start game", error);
+  }
+}
+
+export const Game = () => {
+  useEffect(() => {runGame()}, [])
+
+  const [messages, setMessages] = useState<string[]>([]);
+  const [ws, setWs] = useState<WebSocket | null>(null);
+
+  useEffect(() => {
+    const url = new URL('./ws', location.href);
+    url.protocol = url.protocol.replace('http', 'ws');
+    const socket = new WebSocket(url);
+
+    socket.onopen = () => {
+      console.log('WebSocket connected');
+      // Optionally, send an initial message
+      // socket.send(JSON.stringify({ type: 'greet', message: 'Hello!' }));
+    };
+
+    socket.onmessage = (event) => {
+      console.log('Message from server: ', event.data);
+      setMessages((prevMessages) => [...prevMessages, event.data]);
+    };
+
+    socket.onerror = (error) => {
+      console.error('WebSocket error: ', error);
+    };
+
+    socket.onclose = () => {
+      console.log('WebSocket disconnected');
+    };
+
+    // Save the WebSocket instance for sending messages later
+    setWs(socket);
+
+    setInterval(() => {
+      if (socket.readyState === WebSocket.OPEN) {
+        socket.send(JSON.stringify({ message: 'Ping', timestamp: Date.now() }));
+      }
+    }, 1000); // S
+
+    // Cleanup on unmount
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (ws) {
+      ws.send('Hello, server!');
+    }
+  };
+
+
+  return (<canvas id="game" className="h-full w-full" />);
+}
